08c66a427efcbaa6dae1b0b1cf327e6aeacb70a0
yusufsarikaya023 <yusufsarikaya023@gmail.com>
2024-03-17 01:12:47 +0200

    step1

---
+++ Domain/Aggregation/Common/Entity.cs
@@ -0,0 +1,6 @@
+namespace Domain.Aggregation.Common;
+
+public class Entity
+{
+    public int Id { get; set; }
+}
---
+++ Infrastructure/Migrations/20240302161702_Initialize.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobPost",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_job_post", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobPost");
+        }
+    }
+}
